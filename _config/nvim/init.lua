a6c3d244b43e3bc82c1133083276b8e7e4a10e5c
andrtell <116459057+andrtell@users.noreply.github.com>
2026-03-05 11:48:36 +0100

    cleanup neovim init.lua

--- _config/nvim/init.lua
+++ _config/nvim/init.lua
@@ -39,8 +39,8 @@ end
 do
   local g = vim.g
 
-  g.mapleader = " "
-  g.maplocalleader = ","
+  g.mapleader       = " "
+  g.maplocalleader  = ","
 
   local vks = vim.keymap.set
 
@@ -75,25 +75,28 @@ end
 do
   local g = vim.g
 
-  g.netrw_banner = 0
-  g.netrw_keepdir = 0
+  g.netrw_banner    = 0
+  g.netrw_keepdir   = 0
   g.netrw_list_hide = "\\(^\\|\\s\\s\\)\\zs\\.\\S\\+"
 
-  local api = vim.api
-  local vks = vim.keymap.set
+  local au_group     = vim.api.nvim_create_augroup("netrw-group", { clear = true })
 
-  api.nvim_create_autocmd("FileType", {
-    pattern = { 'netrw' },
-    group = api.nvim_create_augroup("NETRW", { clear = true }),
-    callback = function()
-      local o = { silent = true, buffer = true, remap = true }
+  local au_pattern   = { 'netrw' }
 
-      vks("n", "<esc>", ":Sayonara!<cr>", o)
-      vks("n", "h", "-", o)
-      vks("n", "l", "<cr>", o)
-      vks("n", ".", "gh", o)
-      vks("n", "H", "h", o)
-    end,
+  local au_callback  = function()
+    local opt = { silent = true, buffer = true, remap = true }
+    local vks = vim.keymap.set
+    vks("n", "<esc>", ":Sayonara!<cr>", opt)
+    vks("n", "h", "-", opt)
+    vks("n", "l", "<cr>", opt)
+    vks("n", ".", "gh", opt)
+    vks("n", "H", "h", opt)
+  end
+
+  vim.api.nvim_create_autocmd("FileType", {
+    group     = au_group,
+    pattern   = au_pattern,
+    callback  = au_callback,
   })
 end
 
@@ -104,17 +107,14 @@ end
 do
   vim.pack.add({
     { src = "https://codeberg.org/andyg/leap.nvim.git" },
+    { src = "https://github.com/eraserhd/parinfer-rust.git" },
+    { src = "https://github.com/mason-org/mason-lspconfig.nvim.git" },
+    { src = "https://github.com/mason-org/mason.nvim.git" },
     { src = "https://github.com/mhinz/vim-sayonara" },
+    { src = "https://github.com/neovim/nvim-lspconfig" },
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter.git" },
     { src = "https://github.com/oskarnurm/koda.nvim" },
     { src = "https://github.com/saghen/blink.cmp.git" },
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter.git" },
-    { src = "https://github.com/neovim/nvim-lspconfig" },
-    { src = "https://github.com/mason-org/mason.nvim.git" },
-    { src = "https://github.com/mason-org/mason-lspconfig.nvim.git" },
-    -- { src = "https://github.com/julienvincent/nvim-paredit.git" },
-    { src = "https://github.com/eraserhd/parinfer-rust.git" },
-    -- { src = "https://github.com/Olical/conjure.git" },
-    -- { src = "https://github.com/Olical/nfnl.git" },
   })
 
   local delpkg
@@ -135,7 +135,9 @@ end
 
 do
   local d = vim.diagnostic
+
   d.enable = true
+
   d.config({
     signs = false,
     virtual_text = {
@@ -144,32 +146,23 @@ do
   })
 end
 
--------------
--- PAREDIT --
--------------
-
--- do
---   local paredit = require("nvim-paredit")
---
---   paredit.setup({
---     indent = {
---       enabled = true
---     }
---   })
--- end
-
 ----------------
 -- TREESITTER --
 ----------------
 
 do
-  local api = vim.api
-  api.nvim_create_autocmd("FileType", {
-    pattern = { "go", "lua", "scheme", "fennel" },
-    group = api.nvim_create_augroup("TS", { clear = true }),
-    callback = function()
-      vim.treesitter.start()
-    end,
+  local au_group    = vim.api.nvim_create_augroup("ts-group", { clear = true })
+
+  local au_pattern  = { "go", "lua", "scheme", "fennel" }
+
+  local au_callback = function()
+    vim.treesitter.start()
+  end
+
+  vim.api.nvim_create_autocmd("FileType", {
+    group     = au_group,
+    pattern   = au_pattern,
+    callback  = au_callback,
   })
 end
 
@@ -193,25 +186,36 @@ end
 --------
 
 do
-  vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*.go",
-    callback = function()
-      local params = vim.lsp.util.make_range_params(nil, "utf-16")
-      params.context = {
-        only = {"source.organizeImports"}
-      }
-      local timeout = 1000
-      local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout)
-      for cid, res in pairs(result or {}) do
-        for _, r in pairs(res.result or {}) do
-          if r.edit then
-            local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
-            vim.lsp.util.apply_workspace_edit(r.edit, enc)
-          end
+  local au_group     = vim.api.nvim_create_augroup('go-group', { clear = true })
+
+  local au_pattern   = { '*.go' }
+
+  local au_callback  = function()
+    local params = vim.lsp.util.make_range_params(nil, "utf-16")
+
+    params.context = {
+      only = { 'source.organizeImports' }
+    }
+
+    local timeout = 1000
+    local result = vim.lsp.buf_request_sync(0, 'textDocument/codeAction', params, timeout)
+
+    for cid, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+          vim.lsp.util.apply_workspace_edit(r.edit, enc)
         end
       end
-      vim.lsp.buf.format({async = false})
     end
+
+    vim.lsp.buf.format({async = false})
+  end
+
+  vim.api.nvim_create_autocmd("BufWritePre", {
+    group     = au_group,
+    pattern   = au_pattern,
+    callback  = au_callback
   })
 end
 
@@ -228,8 +232,7 @@ end
 -----------------
 
 do
-  local co = {
-    white  = "#faf9f8",
+  local fg = {
     black  = "#070707",
     gray   = "#9197a2",
     blue   = "#0047a7",
@@ -239,34 +242,38 @@ do
     brown  = "#a06c12",
     red    = "#982717",
     purple = "#8120a6",
+  }
+
+  local bg = {
+    white  = "#faf9f8",
     yellow = "#f0f0d1",
   }
 
   local hi = {
     -- VIM
-    DiagnosticError               = { fg = co.red },
-    Visual                        = { bg = co.yellow },
-    Search                        = { bg = co.yellow },
-    IncSearch                     = { bg = co.yellow },
-    CurSearch                     = { bg = co.yellow },
+    DiagnosticError               = { fg = fg.red },
+    Visual                        = { bg = bg.yellow },
+    Search                        = { bg = bg.yellow },
+    IncSearch                     = { bg = bg.yellow },
+    CurSearch                     = { bg = bg.yellow },
     StatusLine                    = { bg = "#e7e7e7" },
-    Directory                     = { fg = co.blue   },
-    ['@keyword.vim']              = { fg = co.string },
-    ['@variable.builtin.vim']     = { fg = co.string },
-    ['@constant.vim']             = { fg = co.string },
+    Directory                     = { fg = fg.blue   },
+    ['@keyword.vim']              = { fg = fg.string },
+    ['@variable.builtin.vim']     = { fg = fg.string },
+    ['@constant.vim']             = { fg = fg.string },
 
     -- CODE
-    Boolean                       = { fg = co.pink },
-    Float                         = { fg = co.blue },
+    Boolean                       = { fg = fg.pink },
+    Float                         = { fg = fg.blue },
     Function                      = { bold = false },
-    Keyword                       = { fg = co.red, bold = false },
-    Number                        = { fg = co.blue, bold = false },
-    Statement                     = { fg = co.red },
-    String                        = { fg = co.green, italic = false },
-    Type                          = { fg = co.const, italic = false, bold = false },
+    Keyword                       = { fg = fg.red, bold = false },
+    Number                        = { fg = fg.blue, bold = false },
+    Statement                     = { fg = fg.red },
+    String                        = { fg = fg.green, italic = false },
+    Type                          = { fg = fg.const, italic = false, bold = false },
 
     -- LEAP (PLUGIN)
-    LeapLabel                     = { bg = co.yellow },
+    LeapLabel                     = { bg = bg.yellow },
 
     -- LUA
     ['@function.builtin.lua']     = { bold = false },
@@ -274,48 +281,48 @@ do
     ['@function.lua']             = { bold = false },
     ['@lsp.type.function.lua']    = { bold = false },
     ['@lsp.type.method.lua']      = { bold = false },
-    ['@string.escape.lua']        = { fg = co.green },
-    ['@boolean.lua']              = { fg = co.purple },
+    ['@string.escape.lua']        = { fg = fg.green },
+    ['@boolean.lua']              = { fg = fg.purple },
 
     -- SCHEME
-    ['@operator.scheme']          = { fg = co.brown },
-    ['@keyword.scheme']           = { fg = co.red },
-    ['@number.scheme']            = { fg = co.blue },
-    ['@conceal.scheme']           = { fg = co.red },
+    ['@operator.scheme']          = { fg = fg.brown },
+    ['@keyword.scheme']           = { fg = fg.red },
+    ['@number.scheme']            = { fg = fg.blue },
+    ['@conceal.scheme']           = { fg = fg.red },
 
     -- GO
-    ['goFormatSpecifier']         = { fg = co.green },
-    ['goBuiltins']                = { fg = co.red },
+    ['goFormatSpecifier']         = { fg = fg.green },
+    ['goBuiltins']                = { fg = fg.red },
 
-    ['@type.go']                  = { fg = co.black },
+    ['@type.go']                  = { fg = fg.black },
     ['@function.call.go']         = { bold = false },
     ['@function.go']              = { bold = false },
-    ['@function.builtin.go']      = { fg = co.red, bold = true },
-    ['@constant.go']              = { fg = co.black },
-    ['@constant.builtin.go']      = { fg = co.cyan },
+    ['@function.builtin.go']      = { fg = fg.red, bold = true },
+    ['@constant.go']              = { fg = fg.black },
+    ['@constant.builtin.go']      = { fg = fg.cyan },
     ['@function.method.call.go']  = { bold = false },
     ['@function.method.go']       = { bold = false },
-    ['@keyword.return.go']        = { fg = co.red, bold = false },
-    ['@keyword.import.go']        = { fg = co.red, bold = false },
+    ['@keyword.return.go']        = { fg = fg.red, bold = false },
+    ['@keyword.import.go']        = { fg = fg.red, bold = false },
     ['@lsp.type.function.go']     = { bold = false },
-    ['@string.escape.go']         = { fg = co.green },
+    ['@string.escape.go']         = { fg = fg.green },
 
     -- C
-    ['cStorageClass']             = { fg = co.black },
-    ['cInclude']                  = { fg = co.black },
-    ['cFormat']                   = { fg = co.green },
-    ['cSpecial']                  = { fg = co.green },
-    ['cStructure']                = { fg = co.red },
-    ['cOperator']                 = { fg = co.black },
-    ['cBlock']                    = { fg = co.black },
-    ['cConstant']                 = { fg = co.cyan },
+    ['cStorageClass']             = { fg = fg.black },
+    ['cInclude']                  = { fg = fg.black },
+    ['cFormat']                   = { fg = fg.green },
+    ['cSpecial']                  = { fg = fg.green },
+    ['cStructure']                = { fg = fg.red },
+    ['cOperator']                 = { fg = fg.black },
+    ['cBlock']                    = { fg = fg.black },
+    ['cConstant']                 = { fg = fg.cyan },
   }
 
   require("koda").setup({
     colors = {
-      bg = co.white,
-      fg = co.black,
-      info = co.blue,
+      bg    = bg.white,
+      fg    = fg.black,
+      info  = fg.blue,
     },
     on_highlights = function(hl, c)
       for k, v in pairs(hi) do
